008ebe7acf0cbe719732101ab7da8f1050d97e6c
Dustin Slane <info@dustinslane.nl>
2024-10-18 11:29:20 +0000

    Add new colour scheme loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,17 @@ require("bootstrap")
 -- Load the config
 local config_path = vim.fn.stdpath("config")
 
-for _, file in ipairs(vim.fn.readdir(config_path .. "/lua/config", [[v:val =~ '\.lua$']])) do
-	local filename = file:gsub("%.lua$", "")
+---Load all configs in a directory
+---@param dir_name string of directory
+local function load_configs_in_dir(dir_name)
+	for _, file in ipairs(vim.fn.readdir(config_path .. "/lua/" .. dir_name, [[v:val =~ '\.lua$']])) do
+		local filename = file:gsub("%.lua$", "")
 
-	if filename ~= "init" then
-		require("config." .. filename)
+		if filename ~= "init" then
+			require(dir_name .. "." .. filename)
+		end
 	end
 end
+
+load_configs_in_dir("config")
+load_configs_in_dir("colorschemes")

20fad1fcec6d42ff9bd46f1bafeeb2d94d244ff1
Dustin Slane <info@dustinslane.nl>
2024-10-18 11:00:26 +0000

    Keybinds Keybinds Keybinrs

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -22,8 +22,8 @@ map("n", "\\", "<Cmd>Neotree toggle position=left<CR>", opts)
 ----------------------------------------------------
 
 -- Move to previous/next
-map("n", "<A-h>", "<Cmd>BufferLineCyclePrev<CR>", opts)
-map("n", "<A-l>", "<Cmd>BufferLineCycleNext<CR>", opts)
+map("n", "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", opts)
+map("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>", opts)
 -- Re-order to previous/next
 map("n", "<A-<>", "<Cmd>BufferLineMovePrev<CR>", opts)
 map("n", "<A->>", "<Cmd>BufferLineMoveNext<CR>", opts)
@@ -45,5 +45,33 @@ map("n", "<A-c>", "<Cmd>BufDel<CR>", opts)
 
 ----------------------------------------------------
 --- STYLUA
----------------------------------------------------
+----------------------------------------------------
 map("n", "<C-f>", [[<cmd>lua require("stylua-nvim").format_file()<CR>]], opts)
+
+----------------------------------------------------
+--- DEFINITELY NOT STOLEN FROM LAZYGIT
+----------------------------------------------------
+
+
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window"   })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
